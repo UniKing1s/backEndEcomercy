7aff14fc4af56ebb6c89811f5ecafd72895f9306
UniKing1s <khoivo050603it@gmail.com>
2023-12-14 15:56:52 +0700

    fix 2

--- index.js
+++ index.js
@@ -34,13 +34,13 @@ app.use(
 //     credentials: true,
 //   })
 // );
-app.use(
-  cors({
-    origin: ["https://web-shop-nhom5-uniking1s.vercel.app"],
-    methods: ["POST", "PUT", "GET", "DELETE"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["https://web-shop-nhom5-uniking1s.vercel.app"],
+//     methods: ["POST", "PUT", "GET", "DELETE"],
+//     credentials: true,
+//   })
+// );
 // use to show image
 app.use(express.static("public"));
 ////sử dụng multer để sử lí upload file
